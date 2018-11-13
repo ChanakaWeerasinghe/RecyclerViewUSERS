fb90f8ee276af0360509a6380c70eef62d2c0134
Chanaka <chanakaweerasinghe92@gmail.com>
2018-11-13 13:52:14 +0530

    Users

---
+++ app/src/main/java/com/example/chanakaweerasinghe_92/recycle_view/Users.java
@@ -0,0 +1,55 @@
+package com.example.chanakaweerasinghe_92.recycle_view;
+
+/**
+ * Created by ravi on 16/11/17.
+ */
+
+public class Users {
+    String name;
+    String image;
+    String phone;
+    String first_name;
+    String last_name;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    String avatar;
+
+
+    public Users() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
